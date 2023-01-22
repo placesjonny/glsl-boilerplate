9ce46fdf0ed6bbc09a1831d4870c57609b79b4cc
Jonny Baho <jonnybaho@Dator.local>
2023-01-22 15:06:25 +0100

    initial commit

--- fragmentShader.glsl
+++ fragmentShader.glsl
@@ -3,7 +3,21 @@ precision highp float;
 out vec4 outColor;
 
 uniform float RAND_HASH;
+uniform float u_time;
+uniform vec2 u_resolution;
 
 void main() {
-    outColor = vec4(RAND_HASH, 0, 0, 1);
+	vec2 st = gl_FragCoord.xy/u_resolution;
+
+    vec3 color = vec3(0.0, 0.0, 0.0);
+
+    if (RAND_HASH < 0.5) {
+        color = vec3(RAND_HASH, 0.0, 0.0);
+    } else {
+        color = vec3(0.0, RAND_HASH, 0.0);
+    }
+
+    color += st.x;
+
+    outColor = vec4(color, 1);
 }
